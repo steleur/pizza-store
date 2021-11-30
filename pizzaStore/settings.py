95de4cf6d89b16793fc2629f47230e227a922072
= <k.dratsevich@gmail.com>
2021-11-30 19:18:21 +0300

    init

--- pizzaStore/settings.py
+++ pizzaStore/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+8qd)+)3x*0!#uqx&0cj(1(hjmy=wyo^@4=^lq65iq!yot!#+m'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
